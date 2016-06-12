a3a6780c91e87dc94c278fdb77ccfbce084c0104
brady <bobrown101@gmail.com>
2016-06-12 14:40:05 -0400

    pushing to regular phi-tau heroku

--- tools/buildHtml.js
+++ tools/buildHtml.js
@@ -1,6 +1,6 @@
 // This script copies src/index.html into /dist/index.html
 // and adds TrackJS error tracking code for use in production
-// when useTrackJs is set to true below and a trackJsToken is provided.
+// when errLytics is set to true below and a trackJsToken is provided.
 // This is a good example of using Node and cheerio to do a simple file transformation.
 // In this case, the transformation is useful since we only want to track errors in the built production code.
 
@@ -11,8 +11,8 @@ import fs from 'fs';
 import {chalkSuccess, chalkError, chalkWarning} from './chalkConfig';
 import cheerio from 'cheerio';
 
-const useTrackJs = true; // If you choose not to use TrackJS, just set this to false and the build warning will go away.
-const trackJsToken = ''; // If you choose to use TrackJS, insert your unique token here. To get a token, go to https://trackjs.com
+const useErrLytics = true; // If you choose not to use TrackJS, just set this to false and the build warning will go away.
+const errLyticsToken = 'gn4ovuQiFmrX27dnJcSq8JZaUd6uuX0u51APC21JHNA'; // If you choose to use TrackJS, insert your unique token here. To get a token, go to https://trackjs.com
 
 fs.readFile('src/index.html', 'utf8', (readError, markup) => {
   if (readError) {
@@ -24,13 +24,21 @@ fs.readFile('src/index.html', 'utf8', (readError, markup) => {
   // since a separate spreadsheet is only utilized for the production build, need to dynamically add this here.
   $('head').append('<link rel="stylesheet" href="/styles.css">');
 
-  if (useTrackJs) {
-    if (trackJsToken) {
-      const trackJsCode = `<!-- BEGIN TRACKJS Note: This should be the first <script> on the page per https://my.trackjs.com/install --><script>window._trackJs = { token: '${trackJsToken}' };</script><script src=https://d2zah9y47r7bi2.cloudfront.net/releases/current/tracker.js></script><!-- END TRACKJS -->`;
+  if (useErrLytics) {
+    if (errLyticsToken) {
+      const errLytics = `
+      <script type='text/javascript'>
+          window._errLyticsConfig = {
+              apiKey: '${errLyticsToken}'
+          }
+      </script>
+      <script type='text/javascript' crossorigin='anonymous' src='https://cdn.errlytics.com/errlytics.min.js'></script>
+      `;
 
-      $('head').prepend(trackJsCode); // add TrackJS tracking code to the top of <head>
+
+      $('head').prepend(errLytics); // add TrackJS tracking code to the top of <head>
     } else {
-      console.log(chalkWarning('To track JavaScript errors, sign up for a free trial at TrackJS.com and enter your token in /tools/build.html on line 10.'));
+      console.log(chalkWarning('To track JavaScript errors, sign up for errLytics and enter your token in /tools/build.html on line 10.'));
     }
   }
 
