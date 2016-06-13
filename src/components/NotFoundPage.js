458050630b7507810d0ec354a4d43b37ac0d597d
brady <bobrown101@gmail.com>
2016-06-13 16:05:06 -0400

    added the awesome 404 page

--- src/components/NotFoundPage.js
+++ src/components/NotFoundPage.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router';
+import Section from './Section.js';
+import CursiveHeader from './CursiveHeader.js';
+import CenteredContainer from './CenteredContainer.js';
 
 const NotFoundPage = () => {
   return (
-    <div className="not-found-container">
-      <h4>
-        404 Page Not Found
-      </h4>
-      <Link to="/"> Go back to homepage </Link>
-    </div>
+    <Section >
+      {/*<AngledDivider/>*/}
+      <CursiveHeader>404</CursiveHeader>
+      <CenteredContainer>
+        <h3 className="white styled-header">Had one too many drinks and got lost?</h3>
+        <h3 className="white styled-header">But seriously, you're drunk. Go <Link to="/" className="styled-inline-link">home</Link></h3>
+      </CenteredContainer>
+
+
+    </Section>
   );
 };
 
