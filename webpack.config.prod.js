4e6c19bbbd121402b9013394d4638967cbf261a3
brady <bobrown101@gmail.com>
2016-06-28 17:23:39 -0400

    build config stuff

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -14,7 +14,7 @@ export default {
   entry: './src/index',
   target: 'web', // necessary per https://webpack.github.io/docs/testing.html#compile-and-test
   output: {
-    path: `${__dirname}/dist`,
+    path: path.join(__dirname, 'dist'),
     publicPath: '/',
     filename: 'bundle.js'
   },
