4c1d5316a2be806b2bbcd391b6e82ae53d7f8a8a
brady <bobrown101@gmail.com>
2016-07-29 17:30:03 -0400

    animations

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,12 +3,12 @@ import React from 'react';
 const Header = React.createClass({
     render () {
         return (
-            <div id="header-container" className="">
+            <div id="header-container" className="animated fadeIn">
                 {/*<div id="background-image"></div>*/}
                 <div id="header" className="text-center">
                     <h1 className="header-large-text animated fadeInDown">PHI KAPPA TAU</h1>
-                    <h2 className="header-small-text animated fadeIn delay1">Building Men of Character Since 1966</h2>
-                    <h1 className="header-large-text animated fadeInUp fadeIn delay05">GAMMA NU</h1>
+                    <h2 className="header-small-text animated fadeIn">Building Men of Character Since 1966</h2>
+                    <h1 className="header-large-text animated fadeInUp fadeIn">GAMMA NU</h1>
 
                 </div>
             </div>
